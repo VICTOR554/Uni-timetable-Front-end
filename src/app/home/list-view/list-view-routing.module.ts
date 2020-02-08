9b49eeaa3de10c0d4eaf21a2e7af49a87fc30286
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-02-08 23:34:16 +0000

    created list-view page

--- src/app/home/list-view/list-view-routing.module.ts
+++ src/app/home/list-view/list-view-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { ListViewPage } from './list-view.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
-  },
-  {
-    path: 'list-view',
-    loadChildren: () => import('./list-view/list-view.module').then( m => m.ListViewPageModule)
+    component: ListViewPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class ListViewPageRoutingModule {}
