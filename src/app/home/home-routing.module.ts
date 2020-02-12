427c6060a8904659ce5eb73ea3c768c0faba1fd5
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-02-12 21:59:15 +0000

    Rearanged home routing to have child routes

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,28 +3,44 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: HomePage
-  },
-  {
-    path: 'list-view',
-    loadChildren: () => import('./list-view/list-view.module').then( m => m.ListViewPageModule)
-  },
-  {
-    path: 'notes',
-    loadChildren: () => import('./notes/notes.module').then( m => m.NotesPageModule)
-  },
-  {
-    path: 'tasks',
-    loadChildren: () => import('./tasks/tasks.module').then( m => m.TasksPageModule)
-  },
-  {
-    path: 'more',
-    loadChildren: () => import('./more/more.module').then( m => m.MorePageModule)
-  }
-];
+const routes: Routes =
+  [
+    {
+      path: 'tabs',
+      component: HomePage,
+      children:
+        [
+          {
+            path: 'list-view',
+            loadChildren: () => import('./list-view/list-view.module').then(m => m.ListViewPageModule)
+          },
+          {
+            path: 'notes',
+            loadChildren: () => import('./notes/notes.module').then(m => m.NotesPageModule)
+          },
+          {
+            path: 'tasks',
+            loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksPageModule)
+          },
+          {
+            path: 'more',
+            loadChildren: () => import('./more/more.module').then(m => m.MorePageModule)
+          },
+          // makes the path return to list view when path is null
+          {
+            path: '',
+            redirectTo: '/home/tabs/list-view',
+            pathMatch: 'full'
+          }
+        ]
+    },
+    // makes the path return to list view when path is null
+    {
+      path: '',
+      redirectTo: '/home/tabs/list-view',
+      pathMatch: 'full'
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
