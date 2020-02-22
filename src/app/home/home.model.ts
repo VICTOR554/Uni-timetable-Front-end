91bdaaf0c8329d36084104a233994aab380e3322
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-02-22 23:53:09 +0000

    Created class i n model to show the kind of data we will have in my array

---
+++ src/app/home/home.model.ts
@@ -0,0 +1,13 @@
+export class Homes {
+    constructor(
+        public id: string,
+        public date: number,
+        public title: string,
+        public type: string,
+        public start: string,
+        public end: string,
+        public lecturer: string,
+        public location: string,
+        public notetitle: string,
+    ) {}
+}
