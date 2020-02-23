fdc6ccd537979fd262470a3a078dd3e6c035afb4
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-02-23 14:46:49 +0000

    Created home array in home service and created get home method so that home array will not be changed as it duplicates the home array

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { Homes } from './home.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
+  // tslint:disable-next-line: variable-name
+  private _home: Homes[] = [
+    new Homes(
+      'HCI',
+      10,
+      'HCI',
+      'lecture',
+      '12:00 am',
+      '1:00 pm',
+      'Victor Lawuyi',
+      'WLFB',
+      'Calender Problems',
+    ),
+    new Homes(
+      'software',
+      11,
+      'Software Engineering',
+      'lab',
+      '2:00 pm',
+      '3:00 pm',
+      'David Jackson',
+      'HWLL',
+      'Tabs'
+    ),
+    new Homes(
+      'Game',
+      12,
+      'Games and Design',
+      'seminar',
+      '3:00 pm',
+      '4:00 pm',
+      'Travis Scott',
+      'ESGW',
+      'Ionic'
+    ),
+    new Homes(
+      'Advanced',
+      13,
+      'Advanced Topics',
+      'exam',
+      '4:00 pm',
+      '5:00 pm',
+      'Chris Brown',
+      'Lect D',
+      'Block Chain in the supply chain'
+    ),
+    new Homes(
+      'cyber',
+      14,
+      'Cyber Security',
+      'lecture',
+      '5:00 pm',
+      '6:00 pm',
+      'Clark Kent',
+      'Lect 202',
+      'Dissertation'
+    )
+  ];
+
+  get home() {
+    return [...this._home];
+  }
 
   constructor() { }
 }
