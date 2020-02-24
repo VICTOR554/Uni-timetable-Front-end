22a0f4aa5afb4bdb8f4c5296137f0fb813492313
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-02-24 18:41:43 +0000

    import home model and instantiate list as home array

--- src/app/home/list-view/list-view.page.ts
+++ src/app/home/list-view/list-view.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Homes } from '../home.model';
 
 @Component({
   selector: 'app-list-view',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-view.page.scss'],
 })
 export class ListViewPage implements OnInit {
+  list: Homes[];
 
   constructor() { }
 
