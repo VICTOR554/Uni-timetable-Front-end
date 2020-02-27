e96a5a6c2373619f0b496badb14b4157faa9f24c
VICTOR LAWUYI <victor_lawuyi@victors-mbp.mynet>
2020-02-27 21:16:31 +0000

    Inject home service to notes

--- src/app/home/notes/notes.page.ts
+++ src/app/home/notes/notes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../home.service';
+import { Homes } from '../home.model';
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.page.scss'],
 })
 export class NotesPage implements OnInit {
+  Notes: Homes[];
 
-  constructor() { }
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
+    this.Notes = this.homeService.home;
   }
 
 }
