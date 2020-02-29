3c4741943c771fc0ab10dfdea12db0cb7f455a20
VICTOR LAWUYI <victor_lawuyi@victors-mbp.mynet>
2020-02-29 16:38:56 +0000

    Rearrarearrange tasks routing to add children routes for tabs to work

--- src/app/home/tasks/tasks-routing.module.ts
+++ src/app/home/tasks/tasks-routing.module.ts
@@ -5,32 +5,45 @@ import { TasksPage } from './tasks.page';
 
 const routes: Routes = [
   {
+    path: 'tabs2',
+    component: TasksPage,
+    children:
+      [
+        {
+          path: 'all-tasks',
+          loadChildren: () => import('./all-tasks/all-tasks.module').then(m => m.AllTasksPageModule)
+        },
+        {
+          path: 'flags',
+          loadChildren: () => import('./flags/flags.module').then(m => m.FlagsPageModule)
+        },
+        {
+          path: 'overdue',
+          loadChildren: () => import('./overdue/overdue.module').then(m => m.OverduePageModule)
+        },
+        {
+          path: 'completed-tasks',
+          loadChildren: () => import('./completed-tasks/completed-tasks.module').then(m => m.CompletedTasksPageModule)
+        },
+        {
+          path: 'edit-tasks',
+          loadChildren: () => import('./edit-tasks/edit-tasks.module').then(m => m.EditTasksPageModule)
+        },
+        {
+          path: 'new-tasks',
+          loadChildren: () => import('./new-tasks/new-tasks.module').then( m => m.NewTasksPageModule)
+        },
+        {
+          path: '',
+          redirectTo: '/home/tabs/tasks/tabs2/all-tasks',
+          pathMatch: 'full'
+        }
+      ]
+  },
+  {
     path: '',
-    component: TasksPage
-  },
-  {
-    path: 'all-tasks',
-    loadChildren: () => import('./all-tasks/all-tasks.module').then( m => m.AllTasksPageModule)
-  },
-  {
-    path: 'flags',
-    loadChildren: () => import('./flags/flags.module').then( m => m.FlagsPageModule)
-  },
-  {
-    path: 'edit-tasks',
-    loadChildren: () => import('./edit-tasks/edit-tasks.module').then( m => m.EditTasksPageModule)
-  },
-  {
-    path: 'new-tasks',
-    loadChildren: () => import('./new-tasks/new-tasks.module').then( m => m.NewTasksPageModule)
-  },
-  {
-    path: 'overdue',
-    loadChildren: () => import('./overdue/overdue.module').then(m => m.OverduePageModule)
-  },
-  {
-    path: 'completed-tasks',
-    loadChildren: () => import('./completed-tasks/completed-tasks.module').then( m => m.CompletedTasksPageModule)
+    redirectTo: '/home/tabs/tasks/tabs2/all-tasks',
+    pathMatch: 'full'
   },
 ];
 
