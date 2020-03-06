51f5173ecd59cd3f738adaa8e2f5d77108e19ff9
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-03-06 13:50:53 +0000

    Updated home data model and created Note, alltask, completedtask, flag and overdue data models

--- src/app/home/home.model.ts
+++ src/app/home/home.model.ts
@@ -8,6 +8,64 @@ export class Homes {
         public end: string,
         public lecturer: string,
         public location: string,
-        public notetitle: string,
+
+    ) {}
+}
+
+export class Note {
+    constructor(
+        public id: string,
+        public title: string,
+        public modul: string,
+        public description: string,
+        public userId: string,
+    ) {}
+}
+
+export class Alltask {
+    constructor(
+        public id: string,
+        public duedate: Date,
+        public title: string,
+        public modul: string,
+        public description: string,
+        public userId: string,
+
+    ) {}
+}
+
+export class Completedtask {
+    constructor(
+        public id: string,
+        public duedate: Date,
+        public title: string,
+        public modul: string,
+        public description: string,
+        public userId: string,
+
+    ) {}
+}
+
+export class Flag {
+    constructor(
+        public id: string,
+        public duedate: Date,
+        public title: string,
+        public modul: string,
+        public description: string,
+        public userId: string,
+
+    ) {}
+}
+
+export class Overdue {
+    constructor(
+        public id: string,
+        public duedate: Date,
+        public title: string,
+        public modul: string,
+        public description: string,
+        public userId: string,
+
     ) {}
 }
