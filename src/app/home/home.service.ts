2425d20c52539cf652bebfe53b2869054260fb9a
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-03-06 14:53:07 +0000

    Created Note, alltask, completedtask, flag and overdue array based on the models in home.model.ts. Also created get notes, alltask, completed task, overdue and flag method to prevent data manipulation by duplicating the array

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Homes } from './home.model';
+import { Homes, Note, Alltask, Overdue, Flag, Completedtask } from './home.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HomeService {
   // tslint:disable-next-line: variable-name
   private _home: Homes[] = [
     new Homes(
-      'HCI',
+      'CL1',
       10,
       'HCI',
       'lecture',
@@ -16,10 +17,10 @@ export class HomeService {
       '1:00 pm',
       'Victor Lawuyi',
       'WLFB',
-      'Calender Problems',
+
     ),
     new Homes(
-      'software',
+      'CL2',
       11,
       'Software Engineering',
       'lab',
@@ -27,10 +28,9 @@ export class HomeService {
       '3:00 pm',
       'David Jackson',
       'HWLL',
-      'Tabs'
     ),
     new Homes(
-      'Game',
+      'CL3',
       12,
       'Games and Design',
       'seminar',
@@ -38,10 +38,10 @@ export class HomeService {
       '4:00 pm',
       'Travis Scott',
       'ESGW',
-      'Ionic'
+
     ),
     new Homes(
-      'Advanced',
+      'CL4',
       13,
       'Advanced Topics',
       'exam',
@@ -49,10 +49,9 @@ export class HomeService {
       '5:00 pm',
       'Chris Brown',
       'Lect D',
-      'Block Chain in the supply chain'
     ),
     new Homes(
-      'cyber',
+      'CL5',
       14,
       'Cyber Security',
       'lecture',
@@ -60,14 +59,275 @@ export class HomeService {
       '6:00 pm',
       'Clark Kent',
       'Lect 202',
-      'Dissertation'
     )
   ];
 
-  // create get home to duplicate array so changes are not made
+
+  // tslint:disable-next-line: variable-name
+  private _notes: Note[] = [
+    new Note(
+      'N1',
+      'Calender Problems',
+      'HCI',
+      'HCI talks about interaction between the system',
+      'abc'
+
+    ),
+    new Note(
+      'N2',
+      'Tabs',
+      'Software Engineering',
+      'Software Engineering ia about how effective the system is',
+      'abc'
+
+    ),
+    new Note(
+      'N3',
+      'Ionic',
+      'Games and Design',
+      'Teaches you how to manipulate pixels to create games of your own design',
+      'abc'
+
+    ),
+    new Note(
+      'N4',
+      'Block Chain in the supply chain',
+      'Advanced Topics',
+      'Teaches you about cloud service and different data types',
+      'abc'
+
+    ),
+    new Note(
+      'N5',
+      'Dissertation',
+      'Cyber Security',
+      'Teaches you how to create firewalls to stop hackers',
+      'abc'
+    )
+
+  ];
+
+  // tslint:disable-next-line: variable-name
+  private _alltasks: Alltask[] = [
+    new Alltask(
+      'AT1',
+      new Date('2019-02-12'),
+      'Build sentien life',
+      'HCI',
+      'Study the new article of advanced AI',
+      'abc'
+
+
+    ),
+    new Alltask(
+      'AT2',
+      new Date('2020-02-18'),
+      'Check if code smell',
+      'Software Engineering',
+      'Study the the lecture on code smells',
+      'abc'
+
+    ),
+    new Alltask(
+      'AT3',
+      new Date('2020-02-20'),
+      'Research Call of duty',
+      'Games and Design',
+      'Design Call of duty main frame',
+      'abc'
+
+    ),
+    new Alltask(
+      'AT4',
+      new Date('2020-02-24'),
+      'Study cloud service',
+      'Advanced Topics',
+      'Study how is cloud service advancing to the new age',
+      'abc'
+
+    ),
+    new Alltask(
+      'AT5',
+      new Date('2020-02-26'),
+      'Build firewall',
+      'Cyber Security',
+      'Build a firewall to stop the self replecating virus',
+      'abc'
+
+    )
+  ];
+
+  // tslint:disable-next-line: variable-name
+  private _completedtasks: Completedtask[] = [
+    new Completedtask(
+      'CT1',
+      new Date('2019-02-12'),
+      'Build sentien life',
+      'HCI',
+      'Study the new article of advanced AI',
+      'abc'
+
+
+    ),
+    new Completedtask(
+      'CT2',
+      new Date('2020-02-18'),
+      'Check if code smell',
+      'Software Engineering',
+      'Study the the lecture on code smells',
+      'abc'
+
+    ),
+    new Completedtask(
+      'CT3',
+      new Date('2020-02-20'),
+      'Research Call of duty',
+      'Games and Design',
+      'Design Call of duty main frame',
+      'abc'
+
+    ),
+    new Completedtask(
+      'CT4',
+      new Date('2020-02-24'),
+      'Study cloud service',
+      'Advanced Topics',
+      'Study how is cloud service advancing to the new age',
+      'abc'
+
+    ),
+    new Completedtask(
+      'CT5',
+      new Date('2020-02-26'),
+      'Build firewall',
+      'Cyber Security',
+      'Build a firewall to stop the self replecating virus',
+      'abc'
+
+    )
+  ];
+
+  // tslint:disable-next-line: variable-name
+  private _flags: Flag[] = [
+    new Flag(
+      'FT1',
+      new Date('2019-02-12'),
+      'Build sentien life',
+      'HCI',
+      'Study the new article of advanced AI',
+      'abc'
+
+
+    ),
+    new Flag(
+      'FT2',
+      new Date('2020-02-18'),
+      'Check if code smell',
+      'Software Engineering',
+      'Study the the lecture on code smells',
+      'abc'
+
+    ),
+    new Flag(
+      'FT3',
+      new Date('2020-02-20'),
+      'Research Call of duty',
+      'Games and Design',
+      'Design Call of duty main frame',
+      'abc'
+
+    ),
+    new Flag(
+      'FT4',
+      new Date('2020-02-24'),
+      'Study cloud service',
+      'Advanced Topics',
+      'Study how is cloud service advancing to the new age',
+      'abc'
+
+    ),
+    new Flag(
+      'FT5',
+      new Date('2020-02-26'),
+      'Build firewall',
+      'Cyber Security',
+      'Build a firewall to stop the self replecating virus',
+      'abc'
+
+    )
+  ];
+
+  // tslint:disable-next-line: variable-name
+  private _overdues: Overdue[] = [
+    new Overdue(
+      'OT1',
+      new Date('2019-02-12'),
+      'Build sentien life',
+      'HCI',
+      'Study the new article of advanced AI',
+      'abc'
+
+
+    ),
+    new Overdue(
+      'OT2',
+      new Date('2020-02-18'),
+      'Check if code smell',
+      'Software Engineering',
+      'Study the the lecture on code smells',
+      'abc'
+
+    ),
+    new Overdue(
+      'OT3',
+      new Date('2020-02-20'),
+      'Research Call of duty',
+      'Games and Design',
+      'Design Call of duty main frame',
+      'abc'
+
+    ),
+    new Overdue(
+      'OT4',
+      new Date('2020-02-24'),
+      'Study cloud service',
+      'Advanced Topics',
+      'Study how is cloud service advancing to the new age',
+      'abc'
+
+    ),
+    new Overdue(
+      'OT5',
+      new Date('2020-02-26'),
+      'Build firewall',
+      'Cyber Security',
+      'Build a firewall to stop the self replecating virus',
+      'abc'
+
+    )
+  ];
+
+
+// create get home, notes, alltask, completedtask, overdue, flag to duplicate array so changes are not made
   get home() {
     return [...this._home];
   }
+  get notes() {
+    return [...this._notes];
+  }
+  get Alltasks() {
+    return [...this._alltasks];
+  }
+  get Completedtasks() {
+    return [...this._completedtasks];
+  }
+  get Overdue() {
+    return [...this._overdues];
+  }
+  get Flag() {
+    return [...this._flags];
+  }
+
 
   constructor() { }
 }
