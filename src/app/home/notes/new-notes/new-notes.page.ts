f0ba88e3fbae708733aa89422317de8305290a48
VICTOR LAWUYI <victor_lawuyi@VICTORs-MacBook-Pro.local>
2020-03-07 18:02:12 +0000

    Created new-notes form and added validation

--- src/app/home/notes/new-notes/new-notes.page.ts
+++ src/app/home/notes/new-notes/new-notes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-new-notes',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-notes.page.scss'],
 })
 export class NewNotesPage implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      module: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(1800)],
+      }),
+    });
+  }
+
+  onCreateNote() {
+
+    if (!this.form.valid) {
+      return;
+    }
+    console.log(this.form);
   }
 
 }
