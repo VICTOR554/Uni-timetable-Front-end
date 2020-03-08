9bd56abb22385d0fefc60a6622bb0363aa07cb2c
VICTOR LAWUYI <victor_lawuyi@victors-mbp.mynet>
2020-03-08 14:41:20 +0000

    Used string interpolation to bind flags value to the completed task page to view the flag tasks  and injected home service to flags.page.ts to get the array from homeservice. Also added ion item sliding to allow user to delete

--- src/app/home/tasks/flags/flags.page.ts
+++ src/app/home/tasks/flags/flags.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Flag } from '../../home.model';
+import { HomeService } from '../../home.service';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-flags',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlagsPage implements OnInit {
 
-  constructor() { }
+  loadedflag: Flag[];
+
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
+    this.loadedflag = this.homeService.flags;
+  }
+
+  onDelete(flagId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    console.log('delete item', flagId);
+  }
+
+  stop(event: Event) {
+    console.log('stop');
+    event.stopPropagation();
   }
 
 }
