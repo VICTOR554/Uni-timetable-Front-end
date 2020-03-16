9e75c54b0c6d36a26f71081bcb539e82ac84afbf
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2020-03-16 00:58:27 +0000

    Connected the authentication page to the back-end to validate users. Also added an error message

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, AlertController } from '@ionic/angular';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-auth',
@@ -12,33 +15,73 @@ import { NgForm } from '@angular/forms';
 export class AuthPage implements OnInit {
   isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
+  // tslint:disable-next-line: max-line-length
+  constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController, private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
   }
 
-  onLogin() {
-    this.isLoading = true;
-    this.authService.login();
-    this.loadingCtrl
-    .create({keyboardClose: true, message: 'logging in.....'})
-    .then(loadingEl => {
-      loadingEl.present();
-      setTimeout(() => {
-        this.isLoading = false;
-        loadingEl.dismiss();
-        this.router.navigateByUrl('/home/tabs/list-view');
-      }, 1500);
-    });
-  }
-
-  onSubmit(form: NgForm) {
+  Login(form: NgForm) {
+    console.log(form);
     if (!form.valid) {
       return;
     }
     const studentid = form.value.studentid;
     const password = form.value.password;
-    console.log('Id=' + studentid, 'Password=' + password);
+
+    this.isLoading = true;
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'logging in.....' })
+      .then(loadingEl => {
+        loadingEl.present();
+
+        let authObs: Observable<AuthResponseData>;
+        authObs = this.authService.login(studentid, password);
+
+        authObs.subscribe(
+          res => {
+            console.log(res);
+
+            if (res.token) {
+              this.isLoading = false;
+              loadingEl.dismiss();
+              this.router.navigateByUrl('/home/tabs/list-view');
+              this.authService.token(studentid, password);
+              this.authService.loggedin();
+              form.reset();
+            } else {
+              this.isLoading = false;
+              loadingEl.dismiss();
+              console.log('wrong username or password');
+              this.showAlert('wrong username or password');
+            }
+          },
+
+        );
+      });
   }
+
+  private showAlert(message: string) {
+    this.alertCtrl
+      .create({
+        header: 'Authentication failed',
+        message: message,
+        buttons: ['Okay']
+      })
+      .then(alertEl => alertEl.present());
+  }
+
+  // onSubmit() {
+  //   if (!form.valid) {
+  //     return;
+  //   }
+  //   const studentid = form.value.studentid;
+  //   const password = form.value.password;
+
+  //   this.Login(studentid, password);
+  // }
+
+
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { take, map, tap } from 'rxjs/operators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+
+export interface AuthResponseData {
+  token?: string;
+  text?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
+
+
+
 export class AuthService {
 
   // tslint:disable-next-line: variable-name
@@ -11,24 +24,62 @@ export class AuthService {
   // tslint:disable-next-line: variable-name
   private _userId = 'abc';
 
+  // token watch
+  // tslint:disable-next-line: variable-name
+  private _token = '';
+
   // to make sure _userIsAuthenticated can not be overidden any where in the app
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
+  }
+
+  get userToken() {
+    return this._token;
+  }
+
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: this._token
+    })
+  };
+
+
+  token(studentid: number, password: string) {
+    return this.http.post('http://localhost:3000/login', {
+      number: studentid,
+      password
+    }).subscribe(res => {
+      console.log(res.token);
+      this._token = res.token;
+      this.httpOptions.headers = this.httpOptions.headers.set('Authorization', res.token);
+      // console.log(this._token)
+    });
   }
 
   get userId() {
     return this._userId;
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  login() {
-    this._userIsAuthenticated = true;
+  login(studentid: number, password: string) {
+    return this.http.post('http://localhost:3000/login', {
+      number: studentid,
+      password
+    });
   }
 
 
 
   logout() {
+    console.log('before logged out', this._userIsAuthenticated);
     this._userIsAuthenticated = false;
+  }
+
+  loggedin() {
+    console.log('before logged in', this._userIsAuthenticated);
+    this._userIsAuthenticated = true;
   }
 }
