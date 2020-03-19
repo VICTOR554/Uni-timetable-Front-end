dd07063e756163d582af8ebae3d4fe3f07bae6ca
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2020-03-19 23:49:29 +0000

    Created a custom moment pipe to format all my date output from the server. Also created a shared module to use the moment pipe for more than one module

---
+++ src/app/home/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MomentPipePipe } from '../moment-pipe.pipe';
+
+
+
+@NgModule({
+  declarations: [MomentPipePipe],
+  imports: [
+    CommonModule
+  ],
+  exports: [MomentPipePipe]
+})
+export class SharedModule { }
