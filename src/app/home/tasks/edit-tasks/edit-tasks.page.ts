a1ba967291fb0d35926191fcda1e28448f872b8f
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2020-03-26 01:45:21 +0000

    Modified edit task to send a http put request to the server to make changes to an already existing task.

--- src/app/home/tasks/edit-tasks/edit-tasks.page.ts
+++ src/app/home/tasks/edit-tasks/edit-tasks.page.ts
@@ -1,97 +1,113 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { TasksService } from '../tasks.service';
-import { Alltask } from '../tasks.model';
+import { Task } from '../tasks.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavController, LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
+import * as moment from 'moment';
+
 
 @Component({
-  selector: 'app-edit-tasks',
-  templateUrl: './edit-tasks.page.html',
-  styleUrls: ['./edit-tasks.page.scss'],
+    selector: 'app-edit-tasks',
+    templateUrl: './edit-tasks.page.html',
+    styleUrls: ['./edit-tasks.page.scss'],
 })
 export class EditTasksPage implements OnInit, OnDestroy {
-  loadedalltask: Alltask;
-  form: FormGroup;
-  private taskSub: Subscription;
-  date;
+    loadedalltask: Task;
+    form: FormGroup;
+    private taskSub: Subscription;
+    date;
+    datenow;
+    datek;
+    // tslint:disable-next-line: variable-name
+    is_completed = false;
+    // tslint:disable-next-line: variable-name
+    is_flagged = false;
 
 
-  // tslint:disable-next-line: max-line-length
-  constructor(private tasksService: TasksService, private route: ActivatedRoute, private navCtrl: NavController, private loadingCtrl: LoadingController, private router: Router) {
+    // tslint:disable-next-line: max-line-length
+    constructor(private tasksService: TasksService, private route: ActivatedRoute, private navCtrl: NavController, private loadingCtrl: LoadingController, private router: Router) {
 
-  }
-
-  ngOnInit() {
-
-    this.date = new Date().toISOString();
-
-    // Subscribe to changes in route params
-    this.route.paramMap.subscribe(paramMap => {
-      // check if it has the note id if not go back to notes
-      if (!paramMap.has('taskId')) {
-        this.navCtrl.navigateBack('/home/tabs/notes');
-        return;
-      }
-      this.taskSub = this.tasksService.getAlltask(paramMap.get('taskId')).subscribe(alltasks => {
-        this.loadedalltask = alltasks;
-        // load detail of item in form by removing null and calling the title and description
-        this.form = new FormGroup({
-          title: new FormControl(this.loadedalltask.title, {
-            updateOn: 'blur',
-            validators: [Validators.required]
-          }),
-          modul: new FormControl(this.loadedalltask.modul, {
-            updateOn: 'blur',
-            validators: [Validators.required],
-          }),
-          duedate: new FormControl(this.loadedalltask.duedate, {
-            updateOn: 'blur',
-            validators: [Validators.required],
-          }),
-          description: new FormControl(this.loadedalltask.description, {
-            updateOn: 'blur',
-            validators: [Validators.required, Validators.maxLength(180)]
-          })
-        });
-      });
-    });
-  }
-
-  onUpdateTask() {
-
-    if (!this.form.valid) {
-      return;
     }
-    this.loadingCtrl.create({
-      message: 'Updating Task'
-    }).then(loadingEl => {
-      loadingEl.present();
-      this.tasksService.updateAlltask
-        (
-          this.loadedalltask.id,
-          this.form.value.title,
-          this.form.value.modul,
-          this.form.value.duedate,
-          this.form.value.description,
-        )
-        .subscribe(() => {
-          loadingEl.dismiss();
-          this.form.reset();
-          this.router.navigate(['/home/tabs/tasks']);
 
+    ngOnInit() {
+
+        // Subscribe to changes in route params
+        this.route.paramMap.subscribe(paramMap => {
+            // check if it has the note id if not go back to notes
+            if (!paramMap.has('taskId')) {
+                this.navCtrl.navigateBack('/home/tabs/notes');
+                return;
+            }
+            this.taskSub = this.tasksService.getOneTask(paramMap.get('taskId')).subscribe((alltasks: any) => {
+                this.loadedalltask = alltasks;
+                // load detail of item in form by removing null and calling the title and description
+                this.form = new FormGroup({
+                    title: new FormControl(this.loadedalltask.title, {
+                        updateOn: 'blur',
+                        validators: [Validators.required]
+                    }),
+                    module_code: new FormControl(this.loadedalltask.module_code, {
+                        updateOn: 'blur',
+                    }),
+                    due_date_time: new FormControl(this.loadedalltask.due_date_time, {
+                        updateOn: 'blur',
+                        validators: [Validators.required],
+                    }),
+                    body: new FormControl(this.loadedalltask.body, {
+                        updateOn: 'blur',
+                        validators: [Validators.required, Validators.maxLength(180)]
+                    })
+                });
+                this.date = moment.unix(this.loadedalltask.due_date_time).toISOString();
+                this.datenow = moment().toISOString();
+                console.log('date', this.date);
+                console.log('datenow', this.datenow);
+
+            });
         });
-    });
-
-  }
-
-  // used to clear subscription to avoid memory leaks
-  ngOnDestroy() {
-    if (this.taskSub) {
-      this.taskSub.unsubscribe();
     }
-  }
+
+    onUpdateTask() {
+        console.log(this.form);
+        console.log(this.is_completed);
+        console.log(this.is_flagged);
+
+        if (!this.form.valid) {
+            return;
+        }
+        this.loadingCtrl.create({
+            message: 'Updating Task'
+        }).then(loadingEl => {
+            loadingEl.present();
+            this.tasksService.UpdateTask
+                (
+                    this.form.value.title,
+                    this.form.value.module_code,
+                    this.form.value.due_date_time,
+                    this.form.value.body,
+                    this.loadedalltask._id,
+                    this.is_completed,
+                    this.is_flagged
+                )
+                .subscribe(() => {
+                    setTimeout(() => {
+                        loadingEl.dismiss();
+                        this.form.reset();
+                        this.router.navigate(['/home/tabs/tasks']);
+                    }, 1000);
+                });
+        });
+
+    }
+
+    // used to clear subscription to avoid memory leaks
+    ngOnDestroy() {
+        if (this.taskSub) {
+            this.taskSub.unsubscribe();
+        }
+    }
 
 
 
