b9f2feb5a43cfe0bb0e0730a9c6d9351a09c7bbe
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2020-04-03 16:26:57 +0100

    Changed auth guard from canLoad to canActivate because canLoad has a flaw as it does not check the children routes of home.

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   // runs before lazy loaded code
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]
-  ): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.userIsAuthenticated) {
+  // canLoad(
+  //   route: Route,
+  //   segments: UrlSegment[]
+  // ): Observable<boolean> | Promise<boolean> | boolean {
+  //   console.log(this.authService.userIsAuthenticated);
+  //   if (this.authService.userIsAuthenticated !== true) {
+  //     this.router.navigateByUrl('/auth');
+  //   }
+  //   return this.authService.userIsAuthenticated;
+  // }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log(this.authService.userIsAuthenticated);
+    if (this.authService.userIsAuthenticated !== true) {
       this.router.navigateByUrl('/auth');
     }
     return this.authService.userIsAuthenticated;
